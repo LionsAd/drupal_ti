799b60049561621fd8979ad0557bb9707fe2c671
Fabian Franz <github@fabian-franz.de>
2014-11-30 16:39:30 +0100

    Fix simpletest.

--- runners/simpletest/script.sh
+++ runners/simpletest/script.sh
@@ -6,6 +6,12 @@ set -e $DRUPAL_TI_DEBUG
 
 export ARGS=( $DRUPAL_TI_SIMPLETEST_ARGS )
 
+if [ -n "$DRUPAL_TI_SIMPLETEST_GROUP" ]
+then
+        ARGS=( "${ARGS[@]}" "$DRUPAL_TI_SIMPLETEST_GROUP" )
+fi
+
+
 cd "$TRAVIS_BUILD_DIR/../drupal_travis/drupal/"
 { php scripts/run-tests.sh --php $(which php) "${ARGS[@]}" || echo "1 fails"; } | tee /tmp/simpletest-result.txt
 
