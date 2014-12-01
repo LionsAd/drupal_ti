ded07170c99fff84c3cc0b8be10979d7075da65b
Fabian Franz <github@fabian-franz.de>
2014-12-01 12:57:20 +0100

    Try to use drush master for server.

--- environments/drupal-7.sh
+++ environments/drupal-7.sh
@@ -3,14 +3,16 @@
 # Drupal-7 environment variables and functions.
 
 function drupal_ti_install_drupal() {
-	php -d sendmail_path=$(which true) ~/.composer/vendor/bin/drush.php --yes core-quick-drupal --profile=testing --no-server --db-url="$DRUPAL_TI_DB_URL" .
+	drush --yes dl drupal-7 --drupal-project-rename=drupal
+	cd drupal
+	php -d sendmail_path=$(which true) ~/.composer/vendor/bin/drush.php --yes site-install testing --db-url="$DRUPAL_TI_DB_URL"
 }
 
 function drupal_ti_clear_caches() {
 	drush cc all
 }
 
-export DRUPAL_TI_DRUSH_VERSION="drush/drush:6.*"
+export DRUPAL_TI_DRUSH_VERSION="drush/drush:dev-master"
 export DRUPAL_TI_SIMPLETEST_FILE="scripts/run-tests.sh"
 export DRUPAL_TI_MODULES_PATH="sites/all/modules"
 export DRUPAL_TI_DRUPAL_BASE="$TRAVIS_BUILD_DIR/../drupal-7"
