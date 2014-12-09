9ac18d63e4daec4e4c5df5015e0a6008dc0dd98c
Fabian Franz <github@fabian-franz.de>
2014-12-09 13:07:12 +0100

    Add logging framework.

---
+++ functions/logging.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# @file
+# Functionality for logging output to a file or passing through.
+
+#
+# Ensures that the right Drupal version is installed.
+#
+function drupal_ti_log_output() {
+	FILE=$1
+	shift
+
+	LOG_OUTPUT=""
+        if [ "$DRUPAL_TI_DEBUG_FILE_OUTPUT" = "all" ]
+        then
+		LOG_OUTPUT="all"
+	else
+		case "$DRUPAL_TI_DEBUG_FILE_OUTPUT" in *"$FILE"*) LOG_OUTPUT="$FILE" ;; esac
+	fi
+
+	mkdir -p /tmp/travis-logs
+	LOGFILE="/tmp/travis-logs/$FILE.log"
+
+	if [ -n "$LOG_OUTPUT" ]
+	then
+		echo "Logging output to $LOGFILE and stdout."
+		tee "$LOGFILE"
+	else
+		echo "Logging output to $LOGFILE."
+		cat - > "$LOGFILE"
+	fi
+}
