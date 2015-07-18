24aa98a40384438b13ec0fa24fbb4f10d9d2cfba
Fabian Franz <github@fabian-franz.de>
2015-07-18 05:03:26 -0700

    Add option to specify the src directory.

--- runners/phpunit-core/script.sh
+++ runners/phpunit-core/script.sh
@@ -7,6 +7,8 @@ cd "$DRUPAL_TI_DRUPAL_DIR"
 # Find absolute path to modules directory.
 MODULE_DIR=$(cd "$DRUPAL_TI_MODULES_PATH"; pwd)
 
+ARGS=( $DRUPAL_TI_PHPUNIT_ARGS )
+
 # Run core tests
 cd core
-./vendor/bin/phpunit --verbose --debug "$MODULE_DIR/$DRUPAL_TI_MODULE_NAME"
+./vendor/bin/phpunit "${ARGS[@]}" "$MODULE_DIR/$DRUPAL_TI_MODULE_NAME/$DRUPAL_TI_PHPUNIT_CORE_SRC_DIRECTORY"
