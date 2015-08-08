cfe551b0475956a5596a4fbc3245d5034a83e041
Matthew Radcliffe <mradcliffe@softpixel.com>
2015-08-08 15:03:54 -0400

    Issue #38: Add missing change directory for phpunit-core runner

--- runners/phpunit-core/before_script.sh
+++ runners/phpunit-core/before_script.sh
@@ -6,6 +6,9 @@ set -e $DRUPAL_TI_DEBUG
 # Ensure the right Drupal version is downloaded.
 drupal_ti_ensure_drupal
 
+# Change directory to Drupal core directory.
+cd drupal
+
 # Ensure the module is linked into the code base.
 drupal_ti_ensure_module_linked
 
