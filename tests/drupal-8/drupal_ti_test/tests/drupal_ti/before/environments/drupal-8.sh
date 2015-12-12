a5aa11216bbcfde7ff691fe3d48744886ca0a6b6
Fabian Franz <github@fabian-franz.de>
2015-12-12 12:30:36 -0800

    Use standard profile for drupal-8 to make behat work.

--- tests/drupal-8/drupal_ti_test/tests/drupal_ti/before/environments/drupal-8.sh
+++ tests/drupal-8/drupal_ti_test/tests/drupal_ti/before/environments/drupal-8.sh
@@ -3,3 +3,7 @@
 # Drupal-8 environment variables.
 
 export DRUPAL_TI_SIMPLETEST_GROUP="drupal_ti_test_group";
+
+# Override the install profile for Drupal 8.
+# This is needed as behat does not work with minimal right now.
+export DRUPAL_TI_INSTALL_PROFILE="standard";
