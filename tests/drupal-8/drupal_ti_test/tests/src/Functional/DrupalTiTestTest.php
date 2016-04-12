656e55c7e1d99178d38ff7e8bfa6dc9e2ce9e656
Jibran Ijaz <jibran@1198144.no-reply.drupal.org>
2016-04-12 11:33:14 +0500

    Tests for Functional testing. ref PR #75

---
+++ tests/drupal-8/drupal_ti_test/tests/src/Functional/DrupalTiTestTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\Tests\drupal_ti_test\Functional\DrupalTiTestTest;
+ */
+
+namespace Drupal\Tests\drupal_ti_test\Functional;
+
+use Drupal\drupal_ti_test\DrupalTiTest;
+use Drupal\simpletest\BrowserTestBase;
+
+/**
+ * @coversDefaultClass \Drupal\drupal_ti_test\DrupalTiTest
+ *
+ * @group drupal_ti_test
+ */
+class DrupalTiTestTest extends BrowserTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = ['system', 'user', 'drupal_ti_test'];
+
+  /**
+   * @covers ::bar
+   */
+  public function testBrowserTestBase() {
+    $test = new DrupalTiTest();
+    $this->assertEquals('foo', $test->bar());
+  }
+}
