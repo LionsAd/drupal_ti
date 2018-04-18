b43cb2e5502c1eefa76db55098a4b5801f44d714
Nathaniel Hoag <info@nathanielhoag.com>
2018-04-18 17:28:47 -0400

    Fixup: Formatting update

--- functions/phpcs.sh
+++ functions/phpcs.sh
@@ -6,24 +6,24 @@
 # Ensures that phpcs is installed.
 #
 function drupal_ti_ensure_phpcs() {
-	# This function is re-entrant.
-	if [ -r "$TRAVIS_BUILD_DIR/../drupal_ti-phpcs-installed" ]
-	then
-		return
-	fi
+    # This function is re-entrant.
+    if [ -r "$TRAVIS_BUILD_DIR/../drupal_ti-phpcs-installed" ]
+    then
+        return
+    fi
 
-	# Check if drupal/coder is already available.
-	DRUPAL_CODER=$(composer info --no-interaction drupal/coder || echo "")
+    # Check if drupal/coder is already available.
+    DRUPAL_CODER=$(composer info --no-interaction drupal/coder || echo "")
 
-	if [ -z "$DRUPAL_CODER" ]
-	then
-		# Install drupal/coder globally.
-		echo "Installing drupal/coder: $DRUPAL_TI_DRUPAL_CODER_VERSION"
-		composer global require --no-interaction "$DRUPAL_TI_DRUPAL_CODER_VERSION"
-	else
-		echo "phpcs $DRUPAL_TI_DRUPAL_CODER_VERSION is already installed."
-		composer global install --no-interaction
-	fi
+    if [ -z "$DRUPAL_CODER" ]
+    then
+        # Install drupal/coder globally.
+        echo "Installing drupal/coder: $DRUPAL_TI_DRUPAL_CODER_VERSION"
+        composer global require --no-interaction "$DRUPAL_TI_DRUPAL_CODER_VERSION"
+    else
+        echo "phpcs $DRUPAL_TI_DRUPAL_CODER_VERSION is already installed."
+        composer global install --no-interaction
+    fi
 
-	touch "$TRAVIS_BUILD_DIR/../drupal_ti-phpcs-installed"
+    touch "$TRAVIS_BUILD_DIR/../drupal_ti-phpcs-installed"
 }
