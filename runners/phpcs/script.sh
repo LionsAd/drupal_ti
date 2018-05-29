49c92556c1a32426c635f3461289430dee31fe9a
Nathaniel Hoag <info@nathanielhoag.com>
2018-05-29 15:08:51 -0400

    Add default phpcs arguments

--- runners/phpcs/script.sh
+++ runners/phpcs/script.sh
@@ -4,6 +4,16 @@
 
 set -e $DRUPAL_TI_DEBUG
 
-ARGS=( $DRUPAL_TI_PHPCS_ARGS )
+if [[ -n $DRUPAL_TI_PHPCS_ARGS ]]; then
+  ARGS=( $DRUPAL_TI_PHPCS_ARGS )
+else
+  ARGS=(
+    "--colors"
+    "--standard=Drupal,DrupalPractice"
+    "--extensions=php,module,inc,install,test,profile,theme,css,info,txt,md"
+    "--ignore=node_modules,bower_components,vendor"
+    "${TRAVIS_BUILD_DIR}"
+  )
+fi
 
 $HOME/.composer/vendor/bin/phpcs "${ARGS[@]}"
